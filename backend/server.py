970a961e435d537e4ddfdf78e3f00b4e9234fc1f
Goodness <idowugudness@gmail.com>
2025-08-02 22:43:59 +0100

    "Added the sub domian to the server.py file "

--- backend/server.py
+++ backend/server.py
@@ -41,6 +41,7 @@ app.add_middleware(
         "https://app-noozue1hv-idowugudness01-8172s-projects.vercel.app",
         "https://app-swart-three.vercel.app",
         "http://localhost:3000",  # Local development
+        "https://cbt.atcsedu.net",
     ],
     allow_credentials=True,
     allow_methods=["*"],
